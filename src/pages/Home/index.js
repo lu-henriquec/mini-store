d0883410eb374b21fb9568375abed3f46e530755
Lucas Henrique Carlos Domingos <lucaa.henrique.carlos@gmail.com>
2019-10-01 08:25:03 -0300

    add reactotron

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+// import {  } from 'prop-types';
+import { connect } from 'react-redux';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 import { formatPrice } from '../../util/format';
@@ -6,7 +8,7 @@ import api from '../../services/api';
 
 import { ProductList } from './styles';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   };
@@ -22,6 +24,14 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  };
+
   render() {
     const { products } = this.state;
 
@@ -33,7 +43,7 @@ export default class Home extends Component {
             <strong>{prod.title}</strong>
             <span>{prod.priceFormatted}</span>
 
-            <button>
+            <button type='button' onClick={() => this.handleAddProduct(prod)}>
               <div>
                 <MdAddShoppingCart sixe={16} color="#FFF" /> 3
               </div>
@@ -46,3 +56,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,8 @@
-export default function cartReducer() {
-  return [];
+export default function cartReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [...state, action.product];
+    default:
+      return state;
+  }
 }
