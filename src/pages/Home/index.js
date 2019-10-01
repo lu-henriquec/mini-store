b6a626da50f86cbf570fc000bf6b0c393a887333
lu-henriquec <lucaa.henrique.carlos@gmail.com>
2019-09-30 21:46:58 -0300

    setup

---
+++ src/pages/Home/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+import { MdAddShoppingCart } from 'react-icons/md';
+import { formatPrice } from '../../util/format';
+import api from '../../services/api';
+
+import { ProductList } from './styles';
+
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
+
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(prod => ({
+      ...prod,
+      priceFormatted: formatPrice(prod.price),
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(prod => (
+          <li key={prod.id}>
+            <img src={prod.image} alt={prod.title} />
+            <strong>{prod.title}</strong>
+            <span>{prod.priceFormatted}</span>
+
+            <button>
+              <div>
+                <MdAddShoppingCart sixe={16} color="#FFF" /> 3
+              </div>
+
+              <span>Adicionar ao carrinho</span>
+            </button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
+}
